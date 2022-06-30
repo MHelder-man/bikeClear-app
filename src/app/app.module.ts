68fd3bf3451e6af7525785b6bd86bf41da9161f5
Marije Helderman <marije.helderman@student.stenden.com>
2022-06-30 15:43:39 +0200

    week 3 voortgang

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BikeClearFormComponent } from './bike-clear-form/bike-clear-form.component';
+import { BikeClearListComponent } from './bike-clear-list/bike-clear-list.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import { LoginComponent } from './login/login.component';
+import { MapsComponent } from './maps/maps.component';
+import { GoogleMapsModule } from '@angular/google-maps'
+import { BikeUserComponent } from './bike-user/bike-user.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BikeClearFormComponent,
+    BikeClearListComponent,
+    LoginComponent,
+    MapsComponent,
+    BikeUserComponent,
+    RegisterComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        HttpClientModule,
+        GoogleMapsModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
