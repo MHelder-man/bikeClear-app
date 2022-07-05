a93144842ddb0d294fc93e5758cac019791b92d8
Marije Helderman <marije.helderman@student.stenden.com>
2022-07-05 16:27:41 +0200

    week 4 dinsdag.3

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -4,14 +4,32 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-maps',
   templateUrl: './maps.component.html',
   styleUrls: ['./maps.component.css']
+
+
 })
 export class MapsComponent implements OnInit {
 
+  zoom = 14
+  center: google.maps.LatLngLiteral = {lat: 53.2194, lng: 6.5665};
+  options: google.maps.MapOptions = {
+    mapTypeId: 'roadmap',
+    zoomControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    maxZoom: 15,
+    minZoom: 8,
+  }
 
   constructor() {
   }
 
   ngOnInit() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.center = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      }
+    })
 
   }
 
